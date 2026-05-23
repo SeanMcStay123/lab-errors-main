f3b95ef0ef6487b5101322035aa61b94aa1228ed
SeanMcStay123 <20112647@mail.wit.ie>
2026-05-23 18:58:51 +0100

    Added custom helpers

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -19,12 +19,16 @@ const dashboard = {
   },
 
   addPlaylist(request, response) {
-    const newPlayList = {
+    const timestamp = new Date();
+
+    const newPlaylist = {
       id: uuidv4(),
       title: request.body.title,
+      rating: parseInt(request.body.rating),
+      date: timestamp,
       songs: [],
     };
-    playlistStore.addPlaylist(newPlayList);
+    playlistStore.addPlaylist(newPlaylist);
     response.redirect('/dashboard');
   },
 
